03f4e9682d971d4b8ee54cef6f3b02e0b6254e48
Shreejan Shrestha <Hustlingshreejan@gmail.com>
2020-08-27 11:24:17 +0545

    2.complited the registeration part

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
-
+import csv
 
 
 
 
 class Bank_application:
-
+    ''' This function checks whether the username is already present in dataset or not. If its new, will add if not, ask for new username '''
     def register_user(self):
-        print("From function register")
-        provide_username = input('Provide your username:')
-        provide_password = input('Provide your password:')
-        with open('user_info.json', 'r') as check:
-            compare = check.read()
-            for i in compare:
-                print(i)
-                if i == provide_username:
-                    print("User already exits")
-                else:
-                    print('User created')
-
-        # combine = f'provide_username+provide_password'
-        # with open('user_info', 'w') as o:
-        #     o.write(combine)
-
-
-
-
+        username = input('Provide your username:').capitalize()
+        with open(r'C:\Users\dell\Desktop\Dataforprojects\saveddata.csv', 'r') as checking_username:
+            dataset = csv.reader(checking_username)
+            header = next(dataset)
+            for data in dataset:
+                if username == data[0]:
+                    print("\nUsername already exits. Try another username.".upper())
+                    Nepal_Bank.register_user()
+            else:
+                print(f"Your username \"{username}\" has been approved by the bank")
+                password = input('Provide your password:')
+                with open(r'C:\Users\dell\Desktop\Dataforprojects\saveddata.csv', 'a', newline='') as saving_newuser:
+                    writer = csv.writer(saving_newuser)
+                    # writer.writerow(["Username", "Password"])
+                    writer.writerow([username, password])
+                print("\nCongratulation!!!\nYour bank account has been created. Now you can deposit and withdraw the money")
 
 
 if __name__ == '__main__':
@@ -40,11 +37,12 @@ if __name__ == '__main__':
             print("\nDo you want to make an account?")
             user_second = int(input("Type: 1 for Yes 2 for No->"))
             if user_second == 1:
-                print("\nFollow the procedure to register your account")
+                print("\nFollow the procedure to register your account.")
+
                 Nepal_Bank.register_user()
 
             else:
-                print("Thank you for visiting our program")
+                print("Thank you for visiting our bank's system.")
                 exit()
         else:
             print("Select the option carefully...")
